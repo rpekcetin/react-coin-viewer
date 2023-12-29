d9dfb1d2508547ea13b929838dc43850047282dd
Recep Muhammet Pekçetin <recepmuhammetpekcetin@Recep-MacBook-Air.local>
2023-12-29 16:52:48 +0300

    update

--- rollup.config.js
+++ rollup.config.js
@@ -4,11 +4,14 @@ import typescript from '@rollup/plugin-typescript'
 import dts from 'rollup-plugin-dts'
 import postcss from 'rollup-plugin-postcss'
 import image from '@rollup/plugin-image'
+import json from '@rollup/plugin-json'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
 const packageJson = require('./package.json')
 
 export default [
     {
         input: 'src/index.ts',
+        //entry: 'index.tsx',
         output: [
             {
                 file: packageJson.main,
@@ -22,12 +25,22 @@ export default [
             }
         ],
         plugins: [
-            resolve(),
-            commonjs(),
+            commonjs({
+                namedExports: {
+                    'node_modules/react/index.js': ['useState', 'useRef', 'useEffect'],
+                },
+            }),
+            resolve({
+                browser: true,
+                preferBuiltins: false
+            }),
+            nodePolyfills(),
             typescript({ tsconfig: './tsconfig.json' }),
             postcss(),
-            image()
-        ]
+            image(),
+            json()
+        ],
+        external: ["react", "react-dom", "@fontsource/quicksand"],
     },
     {
         input: 'dist/esm/types/index.d.ts',
